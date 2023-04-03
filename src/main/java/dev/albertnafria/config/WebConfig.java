4f06d39517a7d319da216005d4081cfc8a436930
Albert Nafría <albert.nafria@gmail.com>
2023-04-03 23:13:28 +0200

    initial commit

---
+++ src/main/java/dev/albertnafria/config/WebConfig.java
@@ -0,0 +1,11 @@
+package dev.albertnafria.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "dev.albertnafria")
+public class WebConfig {
+}
